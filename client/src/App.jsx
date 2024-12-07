a7bd980f43c54d3388399d337a91070a3804b787
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2024-12-07 11:08:50 +0530

    Completed Basic UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import EmailVerify from './pages/EmailVerify'
+import Login from './pages/Login'
+import ResetPassword from './pages/ResetPassword'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div className='text-xxl'>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/email-verify' element={<EmailVerify/>}/>
+        <Route path='/reset-password' element={<ResetPassword/>}/>
+      </Routes>
+    </div>
   )
 }
 
